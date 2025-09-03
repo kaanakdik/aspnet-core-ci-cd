263eed675d5db3222ae1b5fd02339443db324759
kaanakdik <41643619+kaanakdik@users.noreply.github.com>
2025-09-03 00:14:37 +0000

    program.cs'de değişiklik yapıldı

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ app.UseHttpsRedirection();
 
 var summaries = new[]
 {
-    "Freezin", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"
 };
 
 app.MapGet("/weatherforecast", () =>
